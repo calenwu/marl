4d0ed92f849799ef3c5dafff6b73db33baa6329a
Leander Diaz-Bone <leanderdiazbone@gmail.com>
2024-05-20 17:07:18 +0200

    Worked more

--- decentralized_marl/decentralized_agent.py
+++ decentralized_marl/decentralized_agent.py
@@ -12,7 +12,7 @@ from torch.distributions import Normal
 import actor_critic
 
 class Agent:
-	def __init__(self, state_dim, action_dim, num_agents, agent_id, mu_0, beta):
+	def __init__(self, state_dim, action_dim, num_agents, agent_id, mu_0, beta, discount):
 		self.state_dim = state_dim
 		self.action_dim = action_dim
 		self.num_agents = num_agents
@@ -20,9 +20,8 @@ class Agent:
 		self.mu = mu_0
 		self.beta = beta
 
-		self.discount = 0.98
+		self.discount = discount
 		self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-		print("Using device: {}".format(self.device))
 		#self.memory = ReplayBuffer(self.min_buffer_size, self.max_buffer_size, self.device)
 		self.setup_agent()
 
@@ -30,23 +29,17 @@ class Agent:
 		self.actor = actor_critic.Actor(self.state_dim, self.action_dim).to(self.device)
 		self.critic = actor_critic.Critic(self.state_dim, self.num_agents).to(self.device)
 
-	"""def choose_action(self, s):
-		with torch.no_grad():
-			action_prob = self.actor(s.to(self.device), False)
-		return np.argmax(np.array(action_prob))"""
-
-	def get_action(self, s, train = True) -> np.ndarray:
+	def get_action(self, s):
 		with torch.no_grad():
 			state_tensor = torch.Tensor(np.array(s)).to(self.device)
 			action_prob = self.actor(state_tensor)
 			action = torch.distributions.Categorical(torch.Tensor(action_prob)).sample()
-			#action = np.argmax(np.array(action_prob))
 		return np.atleast_1d(action)
 
 	@staticmethod
 	def run_gradient_update_step(object, loss: torch.Tensor):
 		object.optimizer.zero_grad()
-		loss.backward() #.mean()
+		loss.backward()
 		torch.nn.utils.clip_grad_norm_(object.network.parameters(), 1)
 		object.optimizer.step()
 
@@ -56,25 +49,22 @@ class Agent:
 
 	def update_critic(self, s_t, a_t, s_tn, a_tn, r):
 		delta = r - self.mu + self.critic(s_tn+a_tn)-self.critic(s_t+a_t)
-		self.run_gradient_update_step(self.critic, delta)
-
-	def get_all_actions(self):
-		return range(self.action_dim)
+		delta_2 = r+self.discount*self.critic(s_tn+a_tn)-self.critic(s_t+a_t)
+		self.run_gradient_update_step(self.critic, delta_2)
 
 	def update_actor(self, s_t, a_t):
 		A = self.critic(s_t+a_t)
-		action_probs = self.actor.forward(torch.Tensor(np.array(s_t)).to(self.device))
-		actions = self.get_all_actions()
-		for a_i in actions:
+		action_probs = self.actor.forward(s_t)
+		for a_i in range(self.action_dim):
 			a = a_t
 			a[self.agent_id] = a_i
 			A -= action_probs[a_i]*self.critic(s_t+a)
-		log_prob = torch.log(self.actor.get_prob(s_t, a_t[self.agent_id]))
-		
-		psi = torch.autograd.grad(log_prob, self.actor.parameters(), retain_graph = True)
+		#log_prob = torch.log(self.actor.get_prob(s_t, a_t[self.agent_id]))
+		self.run_gradient_update_step(self.actor, A)
+		"""psi = torch.autograd.grad(log_prob, self.actor.parameters(), retain_graph = True)
 		psi = [A * grad for grad in psi]
 		for param, grad in zip(self.actor.parameters(), psi):
-			param.data.add(self.beta * grad)
+			param.data.add(self.beta * grad)"""
 
 	def get_omega(self):
 		return self.critic.network.parameters()
--- local_algorithm/main.py
+++ local_algorithm/main.py
@@ -9,30 +9,30 @@ from marl_gym.marl_gym.envs.cat_mouse.cat_mouse_ma import *
 from marl_gym.marl_gym.envs.cat_mouse.cat_mouse import *
 
 def train_cat_mouse(num_episodes = 25):
-    n_agents = 2
-    n_mice = 2
-    env = CatMouseMA(n_agents=n_agents, n_mice=n_mice, render_mode='human') #render_mode='human'_agents+3*n_mice, action_dim=1, num_agents=num_agents, env=env)
+    n_agents = 1
+    n_prey = 2
+    env = CatMouseMA(n_agents=n_agents, n_prey=n_prey) #render_mode='human'_agents+3*n_mice, action_dim=1, num_agents=num_agents, env=env)
     agents = []
     for i in range(n_agents):
-        agents.append(local_agent.Agent(n_agents, n_mice, i)) 
+        agents.append(local_agent.Agent(n_agents, n_prey, i)) 
     
     state_approximation = [[], []]
     for ep in range(num_episodes):
         observations, communication = env._get_obs()
-        global_obs = env._get_global_obs()
+        global_obs = env.get_global_obs()
         for i in range(n_agents):
             loc_obs = observations[i]
             agents[i].state_distr.update_estimation_local_observation(loc_obs)
             log_prob_state = agents[i].state_distr.log_prob(global_obs)
             state_approximation[i].append(log_prob_state)
-        next_state, communication, reward, terminated, info = env.step([random.random(), random.random()])
+        next_state, communication, reward, terminated, info = env.step([random.random()])
         if terminated:
             break
         env.render()
     plt.plot(state_approximation[0])
     plt.plot(state_approximation[1])
-    plt.show()
-    #plt.savefig('Plots/state_approx_.png')
+    plt.savefig('Plots/state_approx_.png')
+    
 
 def __main__():
     train_cat_mouse(100)
