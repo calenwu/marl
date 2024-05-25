0567cb7379f0b1fe2604dc2e7f7191066c64c061
Leander Diaz-Bone <leanderdiazbone@gmail.com>
2024-05-25 14:59:44 +0200

    local coorperative navigation

---
+++ local_algorithm/coop_nav_state_distribution.py
@@ -0,0 +1,85 @@
+import sys
+import torch
+from torch.distributions import Normal, Bernoulli, MultivariateNormal
+
+class Coop_Nav_State_Distribution(torch.distributions.distribution.Distribution):
+
+    def __init__(self, num_agents, num_targets, agent_id):
+        super().__init__()
+        self.num_agents = num_agents
+        self.num_targets = num_targets
+        self.agent_id = agent_id
+        self.ini_var = 10
+        self.agent_pos_distribution = []
+        self.target_pos_distribution = []
+        for i in range(self.num_agents):
+            position_mean = torch.Tensor([0.5, 0.5])
+            position_var = torch.Tensor([[self.ini_var, 0],[0, self.ini_var]])
+            agent_pos_i_distribution = MultivariateNormal(position_mean, position_var)
+            self.agent_pos_distribution.append(agent_pos_i_distribution)
+        
+        for i in range(self.num_targets):
+            position_mean = torch.Tensor([0.5, 0.5])
+            position_var = torch.Tensor([[self.ini_var, 0],[0, self.ini_var]])
+            target_pos_i_distribution = MultivariateNormal(position_mean, position_var)
+            self.target_pos_distribution.append(target_pos_i_distribution)
+
+    def log_prob(self, glob_state):
+        lp = 0
+        for i in range(self.num_agents):
+            pos_agent_i = torch.Tensor(glob_state['agents']['position'][i])
+            lp += self.agent_pos_distribution[i].log_prob(pos_agent_i)
+        for i in range(self.num_targets):
+            pos_target_i = torch.Tensor(glob_state['prey']['position'][i])
+            lp += self.mouse_pos_distribution[i].log_prob(pos_target_i)
+        return lp
+    
+    def update_estimation_local_observation(self, loc_obs):
+        loc_obs = loc_obs["agent_0"]
+        for i in range(self.num_agents):
+            self.agent_pos_distribution[i] = MultivariateNormal(torch.Tensor([loc_obs[2], loc_obs[3]]), torch.Tensor([[0.01, 0], [0, 0.01]]))
+            
+        for i in range(self.num_targets):
+            self.target_pos_distribution[i] = MultivariateNormal(torch.Tensor([loc_obs[2]+loc_obs[3], loc_obs[3]+loc_obs[4]]), torch.Tensor([[0.01, 0], [0, 0.01]]))
+    @staticmethod
+    def update_estimation_communication(self, distributions):
+        num_comm = len(distributions)
+        for i in range(self.num_agents):
+            mean = torch.Tensor([0,0])
+            cov_sum = 0
+            min_cov = self.ini_var
+            for j in range(len(num_comm)):
+                cov = distributions[j].agent_pos_distribution[i].covariance_matrix[0][0]
+                mean += distributions[j].agent_pos_distribution[i].mean/cov
+                cov_sum += 1/cov
+                min_cov += min(min_cov, cov)
+            for j in range(len(num_comm)):
+                distributions[j].agent_pos_distribution[i] = MultivariateNormal(mean/cov_sum, torch.Tensor([[min_cov, 0],[0, min_cov]]))
+
+        for i in range(self.num_targets):
+            mean = torch.Tensor([0,0])
+            cov_sum = 0
+            min_cov = self.ini_var
+            for j in range(len(num_comm)):
+                cov = distributions[j].target_pos_distribution[i].covariance_matrix[0][0]
+                mean += distributions[j].target_pos_distribution[i].mean/cov
+                cov_sum += 1/cov
+                min_cov += min(min_cov, cov)
+            for j in range(len(num_comm)):
+                distributions[j].target_pos_distribution[i] = MultivariateNormal(mean/cov_sum, torch.Tensor([[min_cov, 0],[0, min_cov]]))
+
+    
+    def get_belief_state(self):
+        state = []
+        for i in range(self.num_agents):
+            state.append(self.agent_pos_distribution[i].mean[0])
+            state.append(self.agent_pos_distribution[i].mean[1])
+            state.append(self.agent_pos_distribution[i].covariance_matrix[0][0])
+            state.append(self.agent_pos_distribution[i].covariance_matrix[1][1])
+        for i in range(self.num_targets):
+            state.append(self.target_pos_distribution[i].mean[0])
+            state.append(self.target_pos_distribution[i].mean[1])
+            state.append(self.target_pos_distribution[i].covariance_matrix[0][0])
+            state.append(self.target_pos_distribution[i].covariance_matrix[1][1])
+        return state
+        
--- local_algorithm/main.py
+++ local_algorithm/main.py
@@ -3,12 +3,14 @@ import sys
 import torch
 import matplotlib.pyplot as plt
 import os
+from pettingzoo.mpe import simple_spread_v3
 import random
 sys.path.append("../")
 from marl_gym.marl_gym.envs.cat_mouse.cat_mouse_ma import *
 from marl_gym.marl_gym.envs.cat_mouse.cat_mouse import *
 from agent import Agent
 from cat_mouse_state_distribution import Cat_Mouse_State_Distribution
+from coop_nav_state_distribution import Coop_Nav_State_Distribution
 
 def train_cat_mouse(num_episodes = 5000):
     n_agents = 1
@@ -40,9 +42,38 @@ def train_cat_mouse(num_episodes = 5000):
         print(ep_rew)
         eps_rewards.append(ep_rew)
         agent.learn()
-    
 
+
+def train_cooperative_nav(num_episodes):
+    n_agents = 1
+    n_targets = 1
+    env = simple_spread_v3.parallel_env(N=1, max_cycles=10,local_ratio=0.5, continuous_actions=False)#render_mode=render_mode, 
+    state_dim = 8
+    agent = Agent(5, (state_dim,), alpha=0.00003, n_epochs=4, batch_size=16)
+    state_distr = Coop_Nav_State_Distribution(n_agents, n_targets, 0)
+    eps_rewards = []
+    for ep in range(num_episodes):
+        state, _ = env.reset()
+        
+        state_distr.update_estimation_local_observation(state)
+        belief_state = state_distr.get_belief_state()
+        ep_rew = 0
+        discount = 1
+        for i in range(100):
+            action, probs, value, _ = agent.choose_action(belief_state)
+            state_n, reward, done, _, _ = env.step({"agent_0":random.randint(0,4)})
+            ep_rew += discount*reward[0]
+            discount *= 0.99
+            eps_rewards.append(ep_rew)
+            state_distr.update_estimation_local_observation(state_n)
+            belief_state_n = state_distr.get_belief_state()
+            agent.remember(belief_state_n, action, probs, value, reward, done)
+            if np.all(done):
+                break
+        print(ep_rew)
+        eps_rewards.append(ep_rew)
+        agent.learn()
 def __main__():
-    train_cat_mouse(100)
+    train_cooperative_nav(1000)
 
 __main__()
